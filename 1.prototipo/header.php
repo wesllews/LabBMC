eb2ec88fb2d13ecdc670e5765bba911776c7ab27
Weslley dos Santos Silva <47144152+wesllews@users.noreply.github.com>
2020-03-26 00:59:35 -0300

    Desenvolvimento de Header - Navbar
    # Para Fazer (24/03)
    - Estudar componentes Bootstrap
    - Lendo um artigo na página BootstrapBay: [Learn Bootstrap 4 in 14 Days](https://bootstrapbay.com/blog/day-1-bootstrap-4-cdn-and-starter-template/)
    
    # Feito
    - Li sobre os seguintes temas: Typography, Images, Buttons, Navbar.
    - Construindo o arquivo Header.php do site, contendo a Navbar do site.
    - Foi incorporado ao scripts repositórios de Fonts (google fonts) e escolhida de início uma font-family
    - Incoporado repositório de Icons(Fonts Awesome) via link-CSS.

---
+++ 1.prototipo/header.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+
+
+  <head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS DESATIVADO-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    <!-- open-iconic ICONS -->
+    <link href="open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+    <!-- Font Awesome CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Baloo+2|Maven+Pro:900|Noto+Sans+SC:900&display=swap" rel="stylesheet">
+    <style type="text/css">*{font-family: 'Baloo 2', cursive; /*font-family: 'Noto Sans SC', sans-serif; font-family: 'Maven Pro', sans-serif;*/}</style>
+
+    <title>Database</title>
+    <link rel="icon" href="img/dna-solid.svg">
+  </head>
+
+
+
+  <header>
+
+    <!-- NavBar-Class -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+      
+      <!-- Brand -->
+      <a class="navbar-brand font-weight-bold" href="index.php"><i class="fas fa-database text-warning shadow-lg"></i> Database</a>
+
+      <!-- Toggler -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+      <!-- Collapse links-->
+      <div class="collapse navbar-collapse" id="navbar-list">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-book"></i> Studbook</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-dna"></i> Genotypes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-users"></i> About Us</a>
+          </li>
+        </ul>
+      </div>
+
+    </nav>
+  </header>
+
+
+
+  <body>
---
+++ 1.prototipo/home.php
@@ -0,0 +1,30 @@
+<div class="container-fluid shadow" 
+style="background-image: url('img/bg_4.jpg');background-position: center;   background-repeat: no-repeat; background-size: cover; min-height: 500px;" >
+	<br><br><br><br>
+	<br><br><br><br>
+	<br><br><br><br>
+	<div class="row">
+		<div class="offset-1"></div>
+		<div class="col text-warning"><h3>Wellcome to</h3></div>
+	</div>
+	<div class="row">
+		<div class="offset-1"></div>
+		<div class="col text-light"><h1 class="font-weight-bold">Black Lion Tamarin Database</h1></div>
+	</div>
+	<div class="row">
+		<div class="offset-1"></div>
+		<div class="col"><button class="btn btn-outline-warning ">Search</button></div>
+	</div>
+	
+</div>
+<br>
+<div class="alert alert-primary shadow">Primary</div>
+<div class="alert alert-success shadow">Success</div>
+
+<div class="container" style="display:none; ">
+	<h1>Modelo de navbar</h1>
+	<div class="alert alert-primary">
+		<i class="fas fa-anchor fa-2x text-info"></i>
+		<a href="https://mdbootstrap.com/docs/jquery/css/background-image/">Botiniho e transparente</a>
+	</div>
+</div>
