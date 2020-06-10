7625b8f0d53bb541bb9af3b2c59192d923786cb8
wesllews <weslle.ws@gmail.com>
2020-06-10 01:57:42 -0300

    Captivity: Menos recursões, botões sire e dam

--- 1.prototipo/captivity.php
+++ 1.prototipo/captivity.php
@@ -66,14 +66,14 @@ $headersAdicionais =['historic','population','sex','sire','dam','name','alive','
 		);
 
 
-/* SQL Filtros */
+/* SQL Filtros e order*/
 
 	// Table order
 	switch ($column) {
 		case 'identification':
 		case 'sire':
 		case 'dam':
-			$order = " ORDER BY CAST($column AS INT) $sort_order, individual.id $sort_order";
+			$order = " ORDER BY CAST($column AS INT) $sort_order, identification $sort_order, individual.id $sort_order";
 			break;
 
 		case 'population':
@@ -95,9 +95,10 @@ $headersAdicionais =['historic','population','sex','sire','dam','name','alive','
 	$status = $status!=""? " AND alive='$_GET[status]'" : "";
 	$filterpopulation = $filterpopulation!=""? " AND id_institute=$_GET[filterpopulation]" : "";
 
+/* Evita excesso de modal*/
+$institute_population = [];
 
-
-$sql = "SELECT *,individual.id as id FROM `individual` LEFT JOIN status ON individual.id=status.id_individual LEFT JOIN kinship ON kinship.id_individual=individual.id LEFT JOIN institute ON institute.id=status.id_institute WHERE id_category=1";
+$sql = "SELECT *,individual.id as id, individual.name as name FROM `individual` LEFT JOIN status ON individual.id=status.id_individual LEFT JOIN kinship ON kinship.id_individual=individual.id  WHERE id_category=1";
 $sql_pagination = $sql.$sexFilter.$status.$filterpopulation;
 $sql_filter = $sql_pagination.$order.$limit_sql;
 $result_filter = $mysqli->query($sql_filter); 
@@ -107,7 +108,7 @@ $result_filter = $mysqli->query($sql_filter);
 
 <!-- Filtro -->
 	<!-- Button trigger modal -->
-	<button type="button" class="btn btn-warning filter px-3" data-toggle="modal" data-target="#filtro">
+	<button type="button" class="btn btn-warning text-white filter px-3" data-toggle="modal" data-target="#filtro">
 	  <i class="fas fa-filter"></i>
 	</button>
 
@@ -206,29 +207,28 @@ $result_filter = $mysqli->query($sql_filter);
 		</div>
 	</div>
 
-
 <!-- Forms Hiddens-->
-<form method="get" action="" id="formFiltros">
-	<?php foreach($array as $key => $value): ?>
-		<?php if($value != ""):?>
-		<input type="hidden" name="<?php echo $key;?>" value="<?php echo $value;?>">
-		<?php endif;?>
-	<?php endforeach;?>
-</form>
+	<form method="get" action="" id="formFiltros">
+		<?php foreach($array as $key => $value): ?>
+			<?php if($value != ""):?>
+			<input type="hidden" name="<?php echo $key;?>" value="<?php echo $value;?>">
+			<?php endif;?>
+		<?php endforeach;?>
+	</form>
 
 <!-- Pagination -->
-<div class="container mt-4">
-	<?php
-	$result = $mysqli->query($sql_pagination);
-	$total_rows =$result->num_rows;
-	$total_pages = $limit!="All" ? ceil($total_rows / $limit) : 1;
-	$NumLinks= 5;
+	<div class="container mt-4">
+		<?php
+		$result = $mysqli->query($sql_pagination);
+		$total_rows =$result->num_rows;
+		$total_pages = $limit!="All" ? ceil($total_rows / $limit) : 1;
+		$NumLinks= 5;
 
-	$start = (($pag-$NumLinks) > 0) ? ($pag - $NumLinks) : 1;
-	$end = (($pag+$NumLinks) < $total_pages) ? ($pag + $NumLinks) : $total_pages;
-	?>
+		$start = (($pag-$NumLinks) > 0) ? ($pag - $NumLinks) : 1;
+		$end = (($pag+$NumLinks) < $total_pages) ? ($pag + $NumLinks) : $total_pages;
+		?>
 
-	
+		
 		<ul class="pagination pagination-sm justify-content-center">
 	    	<!--First page-->
 	    	<li class="page-item <?php if($pag <= 1){ echo 'disabled'; } ?>">
@@ -252,7 +252,8 @@ $result_filter = $mysqli->query($sql_filter);
 	    	<li class="page-item <?php if($pag >= $total_pages){ echo 'disabled'; } ?>">
 	    		<button class="page-link" type="submit"  onclick="document.getElementsByName('pag')[0].value = '<?php echo $total_pages;?>';" form="formFiltros" >Last</button>
 	    	</li>
-	    </ul></div>
+	    </ul>
+	</div>
 
 <!--Table-->
 <div class="container-fluid">
@@ -286,165 +287,171 @@ $result_filter = $mysqli->query($sql_filter);
 
 	    	<!--Body Table -->
 			<tbody>
-			<?php while($row = $result_filter->fetch_array()):
+			<?php while($row = $result_filter->fetch_array()): ?>
+		    	<tr class="text-center">
+		    		<?php foreach ($header as $value): ?>
+		    			<?php switch($value):
 
-				// Seleciona todos os dados dos indivíduos
-				$sql = "SELECT * FROM `individual` LEFT JOIN kinship ON kinship.id_individual=individual.id
-				WHERE id='$row[id]'";
-				$result = $mysqli->query($sql); ?>
+		    				case 'identification': ?>
+			    				<td scope="row">
+			    					<a class="btn btn-outline-success btn-block border-0" href="individual.php?identification=<?php echo $row[$value];?>"><?php echo $row[$value];?></a>
+			    				</td>
+		    					<?php break;?>
 
-						<?php while($row = $result->fetch_array()): ?> 
-					    	<tr class="text-center">
-					    		<?php foreach ($header as $value): ?>
-					    			<?php switch($value):
+		    				<?php case "historic":
+		    					$sql_historic = "SELECT *, institute.name as institute FROM historic LEFT JOIN events ON historic.id_event=events.id LEFT JOIN institute ON historic.id_institute=institute.id  WHERE id_individual = '$row[id]'";
+		    					$result_historic = $mysqli->query($sql_historic);
+		    					$num = $result_historic->num_rows ?>
 
-					    				case 'identification': ?>
-    				    				<td scope="row">
-    				    					<a class="btn btn-outline-success btn-block border-0" href="individual.php?identification=<?php echo $row[$value];?>"><?php echo $row[$value];?></a>
-    				    				</td>
-    				    				<?php break;?>
+		    					<td scope="row">
+			    					<?php if ($num>=1): ?>
+			    						<div class="card">
+											<div class="btn btn-light" onclick="girar('girar<?php echo $row['identification'];?>')" data-toggle="collapse" data-target="#collapse<?php echo $row['identification'];?>">
+												Historic
+												<div class="badge badge-dark"><?php echo $num;?></div>
+												<i class="fas fa-chevron-up ml-3" id="girar<?php echo $row['identification'];?>"></i>
+											</div>
+											
+				    						<ul class="list-group collapse showAll" id="collapse<?php echo $row['identification'];?>">
+				    							<?php while ($row_historic = $result_historic->fetch_array()): ?>
+				    								<li class="list-group-item">
+				    									<?php echo $row_historic['events'],":" ?>
+				    									<div class="text-warning"><?php echo $row_historic['date'] ?></div>
+				    									<?php echo $row_historic['institute']?>
+				    									<div class="text-secondary"><?php echo $row_historic['observation']?></div>
+				    									
+				    								</li>
+				    							<?php endwhile; ?>
+				    						</ul>
+			    						</div>				    						
+			    					<?php else: ?>
+			    						-
+			    					<?php endif; ?>
+			    				</td>
+		    					<?php break;?>
 
-					    				<?php case "historic":
-					    					$sql_historic = "SELECT *, institute.name as institute FROM historic LEFT JOIN events ON historic.id_event=events.id LEFT JOIN institute ON historic.id_institute=institute.id  WHERE id_individual = '$row[id]'";
-					    					$result_historic = $mysqli->query($sql_historic);
-					    					$num = $result_historic->num_rows ?>
+		    				<?php case 'population':					
+		    					$sql_population = "SELECT * FROM institute WHERE id = '$row[id_institute]';";
+		    					$result_population = $mysqli->query($sql_population);
 
-					    					<td scope="row">
-						    					<?php if ($num>=1): ?>
-						    						<div class="card">
-														<div class="btn btn-light" onclick="girar('girar<?php echo $row['identification'];?>')" data-toggle="collapse" data-target="#collapse<?php echo $row['identification'];?>">
-															Historic
-															<div class="badge badge-dark"><?php echo $num;?></div>
-															<i class="fas fa-chevron-up ml-3" id="girar<?php echo $row['identification'];?>"></i>
+		    					if ($result_population->num_rows > 0): 
+		    						$row_population = $result_population->fetch_array();?>
+		    						<td scope="row">
+		    							<!-- Trigger Modal -->
+		    							<button type="button" class="btn btn-link text-decoration-none" data-toggle="modal" data-target="#institute<?php echo $row['id_institute']; ?>">
+		    							  <?php echo $row_population['abbreviation']; ?>
+		    							</button>
+		    							
+		    							<!-- Modal -->
+		    							<?php if(!in_array($row['id_institute'], $institute_population)):
+		    								array_push($institute_population, $row['id_institute']);?>
+		    								
+											<div class="modal fade" id="institute<?php echo $row['id_institute']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+												<div class="modal-dialog modal-dialog-centered" role="document">
+													<div class="modal-content">
+														<div class="modal-header">
+															<h5 class="modal-title" >Population</h5>
+															<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																<span aria-hidden="true">&times;</span>
+															</button>
 														</div>
-														
-							    						<ul class="list-group collapse showAll" id="collapse<?php echo $row['identification'];?>">
-							    							<?php while ($row_historic = $result_historic->fetch_array()): ?>
-							    								<li class="list-group-item">
-							    									<?php echo $row_historic['events'],":" ?>
-							    									<div class="text-warning"><?php echo $row_historic['date'] ?></div>
-							    									<?php echo $row_historic['institute']?>
-							    									<div class="text-secondary"><?php echo $row_historic['observation']?></div>
-							    									
-							    								</li>
-							    							<?php endwhile; ?>
-							    						</ul>
-						    						</div>				    						
-						    					<?php else: ?>
-						    						-
-						    					<?php endif; ?>
-						    				</td>
-					    				<?php break;?>
+														<div class="modal-body">
+															<table class="table table-sm table-borderless text-left text-capitalize">
+																<tbody>
+																	<tr>
+																		<th>name</th>
+																		<td><?php echo $row_population['name']; ?></td>
+																	</tr>
+																	<tr>
+																		<th>Abbreviation</th>
+																		<td><?php echo $row_population['abbreviation']; ?></td>
+																	</tr>
+																	<tr>
+																		<th>country</th>
+																		<td><?php echo $row_population['country']; ?></td>
+																	</tr>
+																	<tr>
+																		<th>state</th>
+																		<td><?php echo $row_population['state']; ?></td>
+																	</tr>
+																	<tr>
+																		<th>city</th>
+																		<td><?php echo $row_population['city']; ?></td>
+																	</tr>
+																</tbody>
+															</table>
+														</div>
+														<div class="modal-footer">
+															<form action="captivity.php" method="get">
+																<input type="hidden" name="filterpopulation" value="<?php echo $row_population['id'];?>">
+																<input type="hidden" name="fulldata" value="n">
+																<button type="submit" class="btn btn-warning" <?php echo isset($_GET['filterpopulation']) && $_GET['filterpopulation']==$row_population['id'] ? "disabled ":""; ?>>Filter population</button>
+															</form>
+															<button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
+														</div>
+													</div>
+												</div>
+											</div>
+		    							<?php endif; ?>
+		    						</td>
+		    					<?php else: ?>
+		    						<td scope="row">-</td>
+		    					<?php endif; ?>
+		    					<?php break;?>
 
-					    				<?php case 'population': 
-					    					$sql_population = "SELECT * FROM `status` 
-					    					LEFT JOIN institute ON institute.id = status.id_institute
-					    					 WHERE id_individual = '$row[id]'";
-					    					$result_population = $mysqli->query($sql_population);
+		    				<?php case "sire": ?>
+		    				<?php case "dam": 
+		    					$sql_kinship = "SELECT * FROM `individual` WHERE id='$row[$value]'";
+			    				$result_kinship = $mysqli->query($sql_kinship);
+			    				$row_kinship = $result_kinship->fetch_array();?>
+			    				<td scope="row">
+			    					<a class="btn btn-link text-decoration-none text-dark btn-block border-0" href="individual.php?identification=<?php echo $row_kinship['identification'];?>"><?php echo $row_kinship['identification'];?></a>
+			    				</td>
+		    					<?php break;?>
 
-						    					if ($result_population->num_rows > 0): 
-						    						$row_population = $result_population->fetch_array();?>
-						    						<td scope="row">
-						    							<button type="button" class="btn btn-link text-decoration-none" data-toggle="modal" data-target="#<?php echo str_replace(" ","",$row_population['abbreviation'].$row['id']);?>">
-						    							  <?php echo $row_population['abbreviation']; ?>
-						    							</button>
-						    							
-						    							<!-- Modal -->
-						    							<div class="modal fade" id="<?php echo str_replace(" ","",$row_population['abbreviation'].$row['id']);?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-						    							  <div class="modal-dialog modal-dialog-centered" role="document">
-						    							    <div class="modal-content">
-						    							      <div class="modal-header">
-						    							        <h5 class="modal-title" id="exampleModalLongTitle">Population</h5>
-						    							        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						    							          <span aria-hidden="true">&times;</span>
-						    							        </button>
-						    							      </div>
-						    							      <div class="modal-body">
-						    							      	<table class="table table-sm table-borderless text-left text-capitalize">
-						    							      		<tbody>
-						    							      			<tr>
-						    							      				<th>name</th>
-						    							      				<td><?php echo $row_population['name']; ?></td>
-						    							      			</tr>
-						    							      			<tr>
-						    							      				<th>Abbreviation</th>
-						    							      				<td><?php echo $row_population['abbreviation']; ?></td>
-						    							      			</tr>
-						    							      			<tr>
-						    							      				<th>country</th>
-						    							      				<td><?php echo $row_population['country']; ?></td>
-						    							      			</tr>
-						    							      			<tr>
-						    							      				<th>state</th>
-						    							      				<td><?php echo $row_population['state']; ?></td>
-						    							      			</tr>
-						    							      			<tr>
-						    							      				<th>city</th>
-						    							      				<td><?php echo $row_population['city']; ?></td>
-						    							      			</tr>
-						    							      		</tbody>
-						    							      	</table>
-						    							      </div>
-						    							      <div class="modal-footer">
-						    							        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-						    							        <form action="captivity.php" method="get">
-			    							        				<input type="hidden" name="filterpopulation" value="<?php echo $row_population['id'];?>">
-			    							        				<input type="hidden" name="fulldata" value="n">
-			    							        				<button type="submit" class="btn btn-warning" <?php echo isset($_GET['filterpopulation']) && $_GET['filterpopulation']==$row_population['id'] ? "disabled ":""; ?>>Filter population</button>
-						    							        </form>
-						    							        
-						    							      </div>
-						    							    </div>
-						    							  </div>
-						    							</div>
-						    						</td>
-						    					<?php else: ?>
-						    						<td scope="row">-</td>
-						    					<?php endif; ?>
-					    				<?php break;?>
+		    				<?php case 'alive': ?>
+	    						<td scope="row">
+	    							<?php switch($row[$value]):
+	    							case '1': ?>
+	    								<div class="btn text-success" style="cursor:auto;">True</div>
+	    							<?php break;?>
 
-					    				<?php case 'alive': 
-					    					$sql_alive = "SELECT * FROM `status` 
-					    					WHERE id_individual = '$row[id]'";
-					    					$result_alive = $mysqli->query($sql_alive);
+	    							<?php case '0': ?>
+	    								<div class="btn text-danger" style="cursor:auto;" > False</div>
+	    							<?php break;?>
 
-						    					if ($result_alive->num_rows > 0): 
-						    						$row_alive = $result_alive->fetch_array();?>
-						    						<td scope="row">
-						    							<?php echo $row_alive['alive']==1 ? '<div class="text-success">True</div>':'<div class="text-danger">False</div>' ;?>
-						    						</td>
-						    					<?php else: ?>
-						    						<td scope="row">-</td>
-						    					<?php endif; ?>
-					    				<?php break;?>
+	    							<?php default: ?>
+	    								<div class="btn text-secondary" style="cursor:auto;">Unknown</div>
+	    							<?php endswitch; ?>
+	    						</td>
+		    					<?php break;?>
 
-					    				<?php case 'genetics': 
-					    					$sql_genetics = "SELECT * FROM genotype WHERE id_individual = '$row[id]'";
-					    					$result_genetics = $mysqli->query($sql_genetics);
+		    				<?php case 'genetics': 
+		    					$sql_genetics = "SELECT * FROM genotype WHERE id_individual = '$row[id]'";
+		    					$result_genetics = $mysqli->query($sql_genetics);
 
-						    					if ($result_genetics->num_rows > 0): ?>
-						    						<td scope="row">
-						    						<button type="button" class="btn btn-success">Genetics</button>
-						    						<button type="button" class="btn btn-dark">Genomic</button>
-						    						<button type="button" class="btn btn-primary">Statistics</button>
-						    						</td>
-						    					<?php else: ?>
-						    						<td scope="row">
-						    						<button type="button" class="btn btn-secondary disabled">Genetics</button>
-						    						<button type="button" class="btn btn-secondary disabled">Genomic</button>
-						    						<button type="button" class="btn btn-secondary disabled">Statistics</button>
-						    						</td>
-						    					<?php endif; ?>
-					    				<?php break;?>
-					    				
+			    					if ($result_genetics->num_rows > 0): ?>
+			    						<td scope="row">
+			    						<button type="button" class="btn btn-success">Genetics</button>
+			    						<button type="button" class="btn btn-dark">Genomic</button>
+			    						<button type="button" class="btn btn-primary">Statistics</button>
+			    						</td>
+			    					<?php else: ?>
+			    						<td scope="row">
+			    						<button type="button" class="btn btn-secondary disabled">Genetics</button>
+			    						<button type="button" class="btn btn-secondary disabled">Genomic</button>
+			    						<button type="button" class="btn btn-secondary disabled">Statistics</button>
+			    						</td>
+			    					<?php endif; ?>
+		    					<?php break;?>
+		    				
 
-					    				<?php default: ?>
-					    				<td scope="row"> <?php echo $row[$value];?> </td>
-					    				
-					    			<?php endswitch; ?>
-					    		<?php endforeach; ?>
-					    	</tr>
-				    	<?php endwhile; ?>
+		    				<?php default: ?>
+		    				<td scope="row"><div class="btn" style="cursor:auto;"><?php echo $row[$value];?></div></td>
+		    				
+		    			<?php endswitch; ?>
+		    		<?php endforeach; ?>
+		    	</tr>
 			<?php endwhile; ?>
 			</tbody>
     	</table>
