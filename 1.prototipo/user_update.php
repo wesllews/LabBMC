f88da68ef7eef47a09f903170091c82cc931a2fc
wesllews <47144152+wesllews@users.noreply.github.com>
2020-11-28 19:05:08 -0300

    Envia email na primeira avaliação do cadastro

--- 1.prototipo/user_update.php
+++ 1.prototipo/user_update.php
@@ -37,11 +37,51 @@ switch ($_POST['update']) {
 		$id = $_POST['id'];
 		$status = $_POST['status'];
 		$date = date('Y-m-d');
-		$query= "UPDATE `login` SET status = '$status', approval_date='$date' WHERE id='$id';";
-		if ($id != 1) {
-			$result = $mysqli->query($query) or die($mysqli->error);
-		} else{
+
+		// Testa se não é um usuário protegido de alteração
+		if (in_array($id, array(7)) ) {
 			echo "<script>alert('You are not allowed to change the status of that user!');</script>";
+			echo "<script>window.close();</script>";
+			exit;
+		}
+
+		$query= "SELECT * FROM `login` WHERE id='$id';";
+		$result = $mysqli->query($query);
+		$rows = $result->fetch_array();
+
+		//Enviar email notificando a analise do cadastro pela primeira vez apenas
+		if ($rows["approval_date"]=="") {
+
+			$email = $rows['email'];
+			$subject = 'BLT Database: Login request analyzed';
+			$headers = "Content-Type: text/html; charset=ISO-8859-1\r\n";
+			$headers .= "MIME-Version: 1.0\r\n";
+
+			// Vars of CSS
+			$permission = "background-color: #007bff;  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none; display:inline-block; ";
+			$container = " width: 300px; max-height: 300px; margin:10px auto 10px auto; padding:10% 25% 10% 25%; text-align:center; background-color: #f8f9fa;";
+
+			//Email body
+			$message = '<html><body>';
+			$message .= '
+			<div style="'.$container.'">
+				<h1 style="color: #343a40;">The database administrators have analyzed your login request!</h1>
+				<br>
+				<h2 style="color: #343a40;"> Permission status:<br><b style="'.$permission.'">'.ucfirst($status).'</b></h2>
+			</div>';
+			$message .= '</body></html>';
+
+			if (mail($email, $subject, $message, $headers)) {
+				$query= "UPDATE `login` SET status = '$status',approval_date='$date' WHERE id='$id';";
+				$result = $mysqli->query($query) or die($mysqli->error);
+			} else {
+				echo "<script>alert('Something went wrong! Try Again!!');</script>";
+			}
+		}
+		// Se já foi avaliado alguma vez, ele só atualiza o status sem enviar email 
+		else {
+			$query= "UPDATE `login` SET status = '$status' WHERE id='$id';";
+			$result = $mysqli->query($query) or die($mysqli->error);
 		}
 		echo "<script>window.close();</script>";
 	break;
