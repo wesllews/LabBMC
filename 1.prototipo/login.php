9436714c1dfee931c4bdba9d4fc7408e879b6a68
wesllews <47144152+wesllews@users.noreply.github.com>
2020-12-03 01:09:24 -0300

    Separando login em dois scripts

--- 1.prototipo/login.php
+++ 1.prototipo/login.php
@@ -42,52 +42,8 @@ include "connection.php"; ?>
 			<div class="row">
 				<!--Login-->
 				<div class="order-xs-1 col-md-5">
-					<?php 
-					$password = md5($_POST['password']);
-					$query= "SELECT * FROM `login` WHERE email='$_POST[email]' and password='$password';";
-					$result = $mysqli->query($query);
-					$rows =$result->num_rows;
-					if ($rows==1) {
-						$rows = $result->fetch_array();
-
-						if(!in_array($rows['status'], array("requested","denied"))){
-							$_SESSION['login']="sim";
-							$_SESSION['status']=$rows['status'];
-							$_SESSION['email']=$rows['email'];
-							header("Location: index.php");
-						} elseif($rows['status']=="requested") {
-							$_SESSION['login']="nao";
-							?>
-							<div class="alert alert-primary alert-dismissible fade show" role="alert">
-								Your request is under evaluation! We will contact you by email when it is analyzed!
-								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-							<?php
-						} elseif($rows['status']=="denied") {
-							$_SESSION['login']="nao";
-							?>
-							<div class="alert alert-danger alert-dismissible fade show" role="alert">
-								Unfortunately your access was denied! Try to contact our administrators to review your profile.
-								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-							<?php
-						}						
-					}	elseif(isset($_POST['email'])) {
-						$_SESSION['login']="nao";
-						?>
-						<div class="alert alert-danger alert-dismissible fade show" role="alert">
-						  Something went wrong with the <strong>User</strong> or <strong>Password</strong>!
-						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-						    <span aria-hidden="true">&times;</span>
-						  </button>
-						</div>
-						<?php
-					}
-					?>
+					<!-- Alerts and Check login in database -->
+					<?php include "login_backend.php"?>
 
 					<form role="form" method="post" action="login.php">
 						<fieldset>							
