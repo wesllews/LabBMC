17d9901a2f74c30b57b2dd3e59ff7d7faa7e5c59
wesllews <47144152+wesllews@users.noreply.github.com>
2020-12-04 10:20:34 -0300

    Esboço HTML de insert Captivity(Modelo 2)

--- 1.prototipo/captivity_insert.php
+++ 1.prototipo/captivity_insert.php
@@ -7,46 +7,67 @@ include 'header.php';
 	<h4 class="text-warning font-weight-bold">Add Captivity Individuals</h4>
 	<hr>
 </div>
+<div class="container">
+	<div class="row">
+		<!-- Informações sobre o indivíduo-->
+		<div class="col-sm-3">
+			<div class="form-group row">
+				<label>Identification:</label>
+				<input type="text" name="identification" class="form-control" placeholder="e.g. 478, TE064, MAM-0001">
+			</div>
+			<div class="form-group row">
+				<label>Category:</label>
+				<select name="category" class="form-control">
+					<option selected disabled>Choose...</option>
+					<option value="1">Captivity</option>
+					<option value="2">Wild</option>
+				</select>
+			</div>
+			<div class="form-group row">
+				<label>Sex:</label>
+				<select name="sex" class="form-control">
+					<option selected disabled>Choose...</option>
+					<option value="Female">Female</option>
+					<option value="Male">Male</option>
+					<option value="Unknown">Unknown</option>
+				</select>
+			</div>
+			<div class="form-group row">
+				<label>Name:</label>
+				<input type="text" name="name" class="form-control" placeholder="e.g. ROXXANE">
+			</div>
+		</div>
 
-<div class="row">
-	<!-------null------><div class="d-none d-lg-block col-lg-2 my-5 py-5"></div>
-	<div class="col-xs-12 col-lg-8">
-		<form>
+		<!-- Form de histórico-->
+		<div class="offset-sm-1 col-sm-8">
 			<div class="form-group row">
-				<label class="col-sm-2 col-form-label">Identification:</label>
-				<div class="col-sm-10">
-					<input type="text" name="identification" class="form-control" placeholder="e.g. 478, TE064, MAM-0001">
-				</div>
+				<label>Event:</label>
+				<select name="sex" class="form-control">
+					<option selected disabled>Choose...</option>
+					<option value="Birth">Birth</option>
+					<option value="Capture">Capture</option>
+					<option value="Death">Death</option>
+				</select>
 			</div>
 			<div class="form-group row">
-				<label class="col-sm-2 col-form-label">Category:</label>
-				<div class="col-sm-10">
-					<select name="category" class="form-control">
-						<option selected disabled>Category</option>
-						<option value="1">Captivity</option>
-						<option value="2">Wild</option>
-					</select>
-				</div>
+				<label>Date:</label>
+				<input type="date" name="date" class="form-control">
 			</div>
 			<div class="form-group row">
-				<label class="col-sm-2 col-form-label">Sex:</label>
-				<div class="col-sm-10">
-					<select name="sex" class="form-control">
-						<option selected disabled>Sex</option>
-						<option value="Female">Female</option>
-						<option value="Male">Male</option>
-						<option value="Unknown">Unknown</option>
-					</select>
-				</div>
+				<label>Institution:</label>
+				<select name="institution" class="form-control">
+					<option selected disabled>Choose...</option>
+					<option value="1">Dureel</option>
+					<option value="2">Zoo Bauru</option>
+				</select>
 			</div>
 			<div class="form-group row">
-				<label class="col-sm-2 col-form-label">Name:</label>
-				<div class="col-sm-10">
-					<input type="text" name="name" class="form-control" placeholder="e.g. ROXXANE">
-				</div>
+				<label>ID local:</label>
+				<input type="text" name="local_id" class="form-control" placeholder="Identifier at the specific institution">
 			</div>
-		</form>
+		</div>
 	</div>
-	<!-------null------><div class="d-none d-lg-block col-lg-2 my-5 py-5"></div>
 </div>
+
+
 <?php include 'footer.php'; ?>
